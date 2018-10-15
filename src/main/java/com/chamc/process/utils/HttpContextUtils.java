43490aae20cf3b9225477d2a81dfdfc4035d0133
GreedyPirate <18335166203@163.com>
2018-10-15 17:52:10 +0800

    删除druid配置类

--- src/main/java/com/chamc/process/utils/HttpContextUtils.java
+++ src/main/java/com/chamc/process/utils/HttpContextUtils.java
@@ -4,10 +4,15 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class HttpContextUtils {
 
-	public static HttpServletRequest getHttpServletRequest() {
+	public static HttpServletRequest getRequest() {
         return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+	}
+
+	public static HttpSession getSession(){
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
 	}
 }
